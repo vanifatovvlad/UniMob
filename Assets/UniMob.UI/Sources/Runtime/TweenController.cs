69b4448b730d17679544ee3c93623ba1392f293f
VladV <vanifatov.vlad@yandex.ru>
2019-09-19 09:50:47 +0300

    Add ReverseDuration to TweenController

--- Assets/UniMob.UI/Sources/Runtime/TweenController.cs
+++ Assets/UniMob.UI/Sources/Runtime/TweenController.cs
@@ -12,6 +12,7 @@ namespace UniMob.UI
         private float _startTime;
 
         public float Duration { get; }
+        public float ReverseDuration { get; }
 
         public TweenStatus Status { get; private set; }
 
@@ -23,12 +24,12 @@ namespace UniMob.UI
         {
             get
             {
-                var elapsed = Mathf.Clamp01((Time.unscaledTime - _startTime) / Duration);
+                var elapsed = Time.unscaledTime - _startTime;
 
                 switch (Status)
                 {
-                    case TweenStatus.Forward: return elapsed;
-                    case TweenStatus.Reverse: return 1f - elapsed;
+                    case TweenStatus.Forward: return Mathf.Clamp01(elapsed / Duration);
+                    case TweenStatus.Reverse: return Mathf.Clamp01(1f - elapsed / ReverseDuration);
                     case TweenStatus.Completed: return 1f;
                     case TweenStatus.Dismissed: return 0f;
                     default: return 0f;
@@ -38,28 +39,45 @@ namespace UniMob.UI
 
         public ITween<float> View => this;
 
-        public TweenController(float duration)
+        public TweenController(float duration, float? reverseDuration = null)
         {
             if (duration < 0)
             {
-                throw new ArgumentOutOfRangeException(nameof(duration), duration, "Must be greater than zero");
+                throw new ArgumentOutOfRangeException(nameof(duration), duration, "Must be positive or zero");
+            }
+
+            if (reverseDuration.HasValue && reverseDuration < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(reverseDuration), reverseDuration,
+                    "Must positive or zero");
             }
 
             Duration = duration;
+            ReverseDuration = reverseDuration ?? duration;
             Status = TweenStatus.Dismissed;
         }
 
         public Task Forward() => StartInternal(TweenStatus.Forward, Duration);
 
-        public Task Reverse() => StartInternal(TweenStatus.Reverse, Duration);
+        public Task Reverse() => StartInternal(TweenStatus.Reverse, ReverseDuration);
 
         private Task StartInternal(TweenStatus status, float d)
         {
             _finishTimer?.Dispose();
+            _finishTimer = null;
+
             _completer?.TrySetResult(null);
+            _completer = null;
 
             Status = status;
             _startTime = Time.unscaledTime;
+
+            if (Mathf.Approximately(d, 0))
+            {
+                OnFinished();
+                return Task.CompletedTask;
+            }
+
             _finishTimer = Timer.Delayed(d, OnFinished);
             _completer = new TaskCompletionSource<object>();
             return _completer.Task;
@@ -78,7 +96,7 @@ namespace UniMob.UI
                     break;
             }
 
-            _completer.TrySetResult(null);
+            _completer?.TrySetResult(null);
         }
     }
 
