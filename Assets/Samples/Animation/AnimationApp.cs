606d0d79ab36b23a1adbafe20a4070a795f4395c
VladV <vanifatov.vlad@yandex.ru>
2020-03-09 19:48:48 +0300

    Add Animation sample

--- Assets/Samples/Animation/AnimationApp.cs
+++ Assets/Samples/Animation/AnimationApp.cs
@@ -1,31 +1,22 @@
+using UniMob.UI;
 using UniMob.UI.Widgets;
 using UnityEngine;
 
-namespace UniMob.UI.Samples.Animation
+namespace Samples.Animation
 {
     public class AnimationApp : UniMobUIApp
     {
-        [SerializeField] private float duration = 1f;
-        [SerializeField] private FloatTween opacityTween = new FloatTween(0, 1);
-        [SerializeField] private Vector2Tween positionTween = new Vector2Tween(Vector2.up, Vector2.down);
+        public float duration = 1f;
 
-        [SerializeField] private QuaternionTween rotationTween =
-            new QuaternionTween(Quaternion.identity, Quaternion.AngleAxis(90f, Vector3.forward));
+        public FloatTween opacity = new FloatTween(0, 1);
+        public Vector2Tween position = new Vector2Tween(Vector2.up, Vector2.down);
 
         private AnimationController _controller;
-        private IAnimation<float> _opacity;
-        private IAnimation<Vector2> _position;
-        private IAnimation<Quaternion> _rotation;
 
         protected override void Initialize()
         {
             _controller = new AnimationController(duration);
             _controller.AddStatusListener(OnControllerStatusChanged);
-
-            _opacity = opacityTween.Animate(_controller);
-            _position = positionTween.Animate(_controller);
-            _rotation = rotationTween.Animate(_controller);
-
             _controller.Forward();
         }
 
@@ -49,9 +40,8 @@ namespace UniMob.UI.Samples.Animation
                 size: WidgetSize.Stretched,
                 backgroundColor: Color.white,
                 child: new CompositeTransition(
-                    opacity: _opacity,
-                    position: _position,
-                    rotation: _rotation,
+                    opacity: opacity.Animate(_controller),
+                    position: position.Animate(_controller),
                     child: new Container(
                         size: WidgetSize.Fixed(300, 200),
                         backgroundColor: Color.black
