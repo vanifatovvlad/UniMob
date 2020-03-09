85602ca48ef44daab059bfaabf67454cd410561e
VladV <vanifatov.vlad@yandex.ru>
2020-03-09 19:37:10 +0300

    Add HelloWorld sample

---
+++ Assets/Samples/HelloWorld/HelloWorldApp.cs
@@ -0,0 +1,27 @@
+using UniMob.UI;
+using UniMob.UI.Widgets;
+using UnityEngine;
+
+namespace Samples.HelloWorld
+{
+    public class HelloWorldApp : UniMobUIApp
+    {
+        protected override void Initialize()
+        {
+        }
+
+        protected override Widget Build(BuildContext context)
+        {
+            return new Container(
+                backgroundColor: Color.white,
+                size: WidgetSize.Stretched,
+                child: new Text(
+                    value: "Hello World!",
+                    size: WidgetSize.Stretched,
+                    color: Color.blue,
+                    fontSize: 60
+                )
+            );
+        }
+    }
+}
